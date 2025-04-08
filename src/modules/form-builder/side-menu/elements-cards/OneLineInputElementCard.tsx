4b0aed375552d5ff0228af2b31ebafd579965a2c
ahmed amer <ahmed.amer@arabiansystems.net>
2025-04-08 16:29:14 +0200

    Add icon and tool tip

--- src/modules/form-builder/side-menu/elements-cards/OneLineInputElementCard.tsx
+++ src/modules/form-builder/side-menu/elements-cards/OneLineInputElementCard.tsx
@@ -1,3 +1,24 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Minus } from "lucide-react";
 export const OneLineInputElementCard = () => {
-  return <div className="w-12 h-12 bg-accent rounded-sm">1</div>;
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div
+            draggable
+            className="w-12 cursor-grab h-12 bg-accent rounded-sm flex justify-center items-center border border-stone-300"
+          >
+            <Minus className="text-stone-700" />
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>One line Input</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
